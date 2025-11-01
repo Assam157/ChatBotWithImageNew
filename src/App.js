29829c24bf2a15d7fca9bd7f24827b58e67ef63d
Assam157 <asasm8106@gmail.com>
2025-11-01 09:59:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./components/Index";
 import Image from "./components/Image";
 import "./App.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/image" element={<Image />} />
-    </Routes>
-  </Router>
-);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/image" element={<Image />} />
+      </Routes>
+    </Router>
+  );
+}
+
 export default App;
