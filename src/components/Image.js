e9fce605f0ab292e14e0c48c4fc7df3b32bb7b5a
Assam157 <asasm8106@gmail.com>
2025-11-02 11:23:39 +0530

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,4 +1,4 @@
-  import React, { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
 
@@ -148,6 +148,7 @@ const Image = () => {
 };
 
 export default Image;
+
 
 
 
